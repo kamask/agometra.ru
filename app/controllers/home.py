7adf479d747173db3e79584d6ae3b9d54bcc6a14
kamask <s@sha88.ru>
2020-12-21 18:28:56 +0300

    Block exact verstka done, process programming

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,7 +1,22 @@
 from ..templates import tpl
+from ..db import db
 
 async def home(request):
-    return tpl.TemplateResponse('index.html', {
-            'request': request,
-            'title': 'Купить футболки оптом'
-        })
+	exact_sizes = dict()
+	raw = await db.fetch_all('''SELECT d.code as density, s.euro as size, a, b, c, d FROM exact_sizes as es 
+							  						JOIN density as d ON es.density_id = d.id
+														JOIN sizes as s ON es.size_id = s.id''')
+
+	for i in raw:
+		if not str(i['density']) in exact_sizes: exact_sizes[str(i['density'])] = dict()
+		if not i['size'] in exact_sizes[str(i['density'])]: exact_sizes[str(i['density'])][i['size']] = dict()
+		exact_sizes[str(i['density'])][i['size']]['A'] = i['a']
+		exact_sizes[str(i['density'])][i['size']]['B'] = i['b']
+		exact_sizes[str(i['density'])][i['size']]['C'] = i['c']
+		exact_sizes[str(i['density'])][i['size']]['D'] = i['d']
+		
+	return tpl.TemplateResponse('index.html', {
+			'request': request,
+			'title': 'Купить футболки оптом',
+			'exact_sizes': exact_sizes
+		})
